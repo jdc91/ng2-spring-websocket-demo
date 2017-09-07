d09b60106a47fb27975a90e3e24feb4bd63e273f
Dehan de Croos <dehan.decroos@proavos.com>
2017-09-07 13:36:23 +0530

    Implementing websocket for HTTPS

--- src/main/resources/static/app/app.component.ts
+++ src/main/resources/static/app/app.component.ts
@@ -1,35 +1,39 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {$WebSocket} from './angular2-websocket';
 
 @Component({
-  selector: 'my-app',
-  template: `
+    selector: 'my-app',
+    template: `
 	Counter Value is: {{counter}}
 	<button type="button" (click)="subscribe($event)">Subscribe to WebSocket</button>
 	`
 })
 export class AppComponent {
 
-  counter: string = 'not known';
-  ws: $Websocket;
-  constructor() {
-    this.ws = new $WebSocket("ws://localhost:8088/counter");
-  }
+    counter: string = 'not known';
+    ws: any;
 
-  subscribe($event) {
-    console.log("trying to subscribe to ws");
-    this.ws = new $WebSocket("ws://localhost:8088/counter");
-    this.ws.send("Hello");
-    this.ws.getDataStream().subscribe(
-      res => {
-        var count = JSON.parse(res.data).value;
-        console.log('Got: ' + count);
-        this.counter = count;
-      },
-      function(e) { console.log('Error: ' + e.message); },
-      function() { console.log('Completed'); }
-    );
-  }
+    constructor() {
+    }
+
+    subscribe($event) {
+        console.log("trying to subscribe to ws");
+        this.ws = new $WebSocket("wss://localhost:8080/counter");
+        this.ws.send("Hello");
+        this.ws.getDataStream().subscribe(
+            res => {
+                var count = JSON.parse(res.data).value;
+                console.log('Got: ' + count);
+                this.counter = count;
+            },
+            function (e) {
+                console.log('Error: ' + e.message);
+            },
+            function () {
+                console.log('Completed');
+            }
+        );
+    }
 }
 
 /*
